b27c8de049cee3d594899388f7a4fb73146f2ae8
Rohit yadav <ry191546@gmail.com>
2026-09-05 21:13:50 +0530

    Add files via upload

---
+++ sums.py
@@ -0,0 +1,12 @@
+num1=int(input("Enter first number :"))
+num2=int(input("Enter second number:"))
+
+result=num1+num2
+
+print("total =:",result)
+
+print("division:",num1/num2)
+
+print("Multipliction:",num1*num2)
+
+print("Subtraction:",num1-num2)
---
+++ type_conversion.py
@@ -0,0 +1,4 @@
+num1="100"
+num2="200"
+
+print(int(num1)+int(num2))
