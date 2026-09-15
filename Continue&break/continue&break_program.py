83b1be2b1d4515d5c85170bc3cebe3d2a50b3d85
Rohit yadav <ry191546@gmail.com>
2026-09-15 21:20:25 +0530

    Add files via upload

---
+++ Continue&break/continue&break_program.py
@@ -0,0 +1,16 @@
+# Take numbers as input from the user one by one.
+# Skip negative numbers and keep adding the positive ones.
+# Stop when the user enters 0 and print the total.(Uses both continue and break.)
+
+
+total=0
+
+while True:
+    num=int(input("Enter the number you want: "))
+    if num == 0:
+        break
+    if num<0:
+        continue
+    total +=num
+
+print(total)    
